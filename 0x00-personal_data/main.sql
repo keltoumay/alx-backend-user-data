d3fd1fe58eef30eca57a33f4028089e32de5193a
keltoumay <keltoumaouyahia@gmail.com>
2024-05-29 12:03:29 -0700

    personal_data

--- 0x00-personal_data/main.sql
+++ 0x00-personal_data/main.sql
@@ -13,27 +13,3 @@ CREATE TABLE users (
 
 INSERT INTO users(email) VALUES ("bob@dylan.com");
 INSERT INTO users(email) VALUES ("bib@dylan.com");
-
-bob@dylan:~$ 
-bob@dylan:~$ cat main.sql | mysql -uroot -p
-Enter password: 
-bob@dylan:~$ 
-bob@dylan:~$ echo "SELECT COUNT(*) FROM users;" | mysql -uroot -p my_db
-Enter password: 
-2
-bob@dylan:~$ 
-bob@dylan:~$ cat main.py
-#!/usr/bin/env python3
-"""
-Main file
-"""
-
-get_db = __import__('filtered_logger').get_db
-
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
